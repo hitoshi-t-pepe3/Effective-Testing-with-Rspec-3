4b54dd58ab61160d54e6829f0a8e22b8937c000a
pepechoko <hitoshi.t.pepe3@gmail.com>
2018-06-08 15:41:41 +0900

    finish shareing object:

---
+++ 01-getting-started/08/spec/sandwich_spec.rb
@@ -0,0 +1,19 @@
+Sandwich = Struct.new(:taste, :toppings)
+
+def sandwich
+  @sandwich ||= Sandwich.new('delicious', [])
+end
+
+RSpec.describe 'An ideal sandwich' do
+  it 'is delicious' do
+    taste = sandwich.taste
+    expect(taste).to eq('delicious')
+  end
+
+  it 'lets me add tappings' do
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
+
+    expect(toppings).not_to be_empty
+  end
+end
